539489fdc54ef8b12e3c761eb45ca6bae1fe348e
Anatrizz1 <cerq1205ana@gmail.com>
2024-09-10 19:46:48 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,18 @@
+let dados = [
+    {
+        titulo: "Talvez você deva conversar com alguém",
+        descricao: "e modo geral, buscamos a ajuda de um terapeuta para melhor compreender as angústias, os medos, a culpa ou quaisquer outros sentimentos que nos causam desconforto e sofrimento. Mas quantos de nós já paramos para perguntar: o terapeuta está imune à gama de questões que ele auxilia seus pacientes a dirimir e superar, dia após dia? A autora best-seller e terapeuta Lori Gottlieb nos mostra que a resposta a essa pergunta traz revelações surpreendentes.",
+        link: "https://www.amazon.com.br/Talvez-voc%C3%AA-deva-conversar-algu%C3%A9m/dp/6599039812/ref=asc_df_6599039812/?tag=googleshopp00-20&linkCode=df0&hvadid=709883381728&hvpos=&hvnetw=g&hvrand=17446267186179173322&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9198390&hvtargid=pla-912707527393&psc=1&mcid=5c0fab5129f13763a93bc92b9a51c318&gad_source=1"
+    },
+    
+    {
+        titulo: "Memórias do subsolo",
+        descricao: "Alienado da sociedade e paralisado pelo peso da própria insignificância, o narrador deste livro conta a história de sua conturbada vida. Com fina ironia, ele relata sua recusa em se tornar mais um trabalhador e seu gradual exílio da sociedade que o cerca.",
+        link: "https://www.amazon.com.br/Mem%C3%B3rias-do-subsolo-Fi%C3%B3dor-Dostoi%C3%A9vski/dp/8582852479/ref=sr_1_1?adgrpid=127819548369&dib=eyJ2IjoiMSJ9.0HmQ7srKwvYsGIPqhMcIfIW7WI9WmDxhwWXnZnU9ObkOgq-8v-UjvdiGUkjC6anuDGvG0ivCZp-d6RKAoOvaeKaB0JRrQ3U9L29Gcy4ritnAEAad-riuHnbhqDI0vdwqmk0WWDHVbP4AF_DG0KLAdN41Znyq1DSlFvlWMmv99oCte85E6_MLaUyVXDHA27Hq9RxrZ7YasDaHIE9MOoQg_Q.G0CeiMGBc1_BScEekO0wRkCPfxogo0rhv1AeEhDd2a8&dib_tag=se&hvadid=595860141357&hvdev=c&hvlocphy=9198390&hvnetw=g&hvqmt=e&hvrand=12445922228247906734&hvtargid=kwd-393118249322&hydadcr=19422_13406614&keywords=mem%C3%B3rias+do+subsolo&qid=1725882588&sr=8-1"
+    },
+    {
+        titulo: "Textos para tocar cicatrizes",
+        descricao: "Com mais de 1 milhão de exemplares vendidos, Igor Pires lança sua mais nova obra, Textos para tocar cicatrizes, em que se debruça nos sentimentos que perpassam os relacionamentos e faz reflexões sobre sociedade, luto e família. Nesta coletânea de textos poéticos inéditos, Igor mostra a importância de honrar as cicatrizes que adquirimos com nossas experiências e que elas são responsáveis pelas histórias que contamos.",
+        link: "https://www.amazon.com.br/Textos-para-tocar-cicatrizes-cru%C3%A9is/dp/658813171"
+    }
+];
